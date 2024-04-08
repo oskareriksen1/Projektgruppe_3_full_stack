efc04ef20b227f7f3f140ad0b3c278ba007ba5d1
Oskar Eriksen <oska1001@stud.kea.dk>
2024-04-08 10:17:32 +0200

    første push

---
+++ src/main/java/dk/kea/projektgruppe_3_full_stack/Projektgruppe3FullStackApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.projektgruppe_3_full_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projektgruppe3FullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projektgruppe3FullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/projektgruppe_3_full_stack/Projektgruppe3FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.projektgruppe_3_full_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projektgruppe3FullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
