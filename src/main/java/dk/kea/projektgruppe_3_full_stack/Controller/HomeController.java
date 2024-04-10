b4279d2a1bf42d894eb42b7ae42626e27b9842c5
Askethemervin <Aske.johanson@gmail.com>
2024-04-10 14:28:51 +0200

    header

--- src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
+++ src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomeController {
 
+@GetMapping("/Header")
+public String Header() {
+    return "Header";
+}
+
    @GetMapping ("/opretbruger")
     public String opretBruger () {
        return "OpretBruger";
