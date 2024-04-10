c1388430cfd0ea9045cbfba49a82c5f0d98bb034
HaugeFrom <haugefrom@haugefrom.com>
2024-04-10 14:28:20 +0200

    loginside

--- src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
+++ src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
@@ -1,5 +1,17 @@
 package dk.kea.projektgruppe_3_full_stack.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class HomeController {
-    // kommentar
+    @GetMapping("/loginside")
+    public String loginside () {
+        return "loginside";
+    }
+
+   @GetMapping ("/opretbruger")
+    public String opretBruger () {
+       return "OpretBruger";
+   }
+
 }
