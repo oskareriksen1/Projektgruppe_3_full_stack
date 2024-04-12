e4252e59537e10cec1df5417b6a50f534f189a16
uSerenOG <masv0002@stud.kea.dk>
2024-04-12 10:52:41 +0200

    Nyt

--- src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
+++ src/main/java/dk/kea/projektgruppe_3_full_stack/Controller/HomeController.java
@@ -1,4 +1,12 @@
 package dk.kea.projektgruppe_3_full_stack.Controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UIcontroller {
+
 }
+@GetMapping("/Forside")
+public String visForside() { return "Forside";}
+
